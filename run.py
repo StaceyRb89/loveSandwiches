7eded76c30e1da4191008ac2c8696998671d89bd
Stacey robson <stacey.robson.sr@gmail.com>
2023-10-30 15:14:01 +0000

    ensure correct number of values are added with accurate error messages

--- run.py
+++ run.py
@@ -31,6 +31,7 @@ def validate_data(values):
     or if tget arent exactly 6 values
     """
     try:
+        [int(value) for value in values]
         if len(values) !=6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
